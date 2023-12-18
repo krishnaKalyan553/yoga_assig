f50f7493cbc640a3b46e2bc7b933de67374a5d6e
kalyan <nyshadhamkrishnakalyan2@gmail.com>
2023-12-19 01:34:48 +0530

    frontend app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import Home from './home'
+import Payment from './payment';
+import ErrorPage from './Error';
+import Thankyou from './Thankyou';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>    
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/payment" element={<Payment  />} />
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="/thanks" element={<Thankyou />} />
+      </Routes>
+    </Router>
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ src/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Bad Response</h1>
+      <p>Oops! Something went wrong.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
